2f1f19f1eb941d0d0b5351a58d3e7f91b78df1c1
ayu0819 <ayutateishi0819@gmail.com>
2020-12-03 23:28:05 +0900

    first_commit

--- travel_pay/header.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", ()　=> {
-    let header = document.querySelector("header"); 
-    header.classList.toggle("sticky",window.scrollY > 0);
- })
--- travel_pay/top-scroll.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".top-scroll")
-
-window.addEventListener("scroll" , () => {
-    if(window.pageYOffset > 100){
-        toTop.classList.add("active");
-    }else{
-        toTop.classList.remove("active");
-    }
-})
